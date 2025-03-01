5a1a9aefd2bc4ab51ebd3ab245f619c971911e11
inhibitor1217 <inhibitor@kaist.ac.kr>
2025-03-01 11:22:39 +0900

    feat: initialize flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# moru_app
+
+`moru` is a note-taking app that supports local-first, but syncs with multiple devices in the LAN.
---
+++ android/app/src/main/kotlin/com/example/moru_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.moru_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
